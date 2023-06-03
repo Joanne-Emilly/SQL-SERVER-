b3eb7c2b897812c01b14e440b2b3dbb442883614
JOANNE EMILLY SANTOS DA COSTA <124714961+Joanne-Emilly@users.noreply.github.com>
2023-06-03 11:45:41 -0300

    Comando Basicos em SQL

--- Comandos-Básicos.sql
+++ Comandos-Básicos.sql
@@ -1,56 +1,21 @@
+SELECT * FROM
+Person.Person;
 
-SELECT * FROM person.Person;
-
-SELECT Title 
-FROM person.Person;
+SELECT title FROM
+Person.Person;
 
 SELECT * FROM
-person.EmailAddress;
+Person.EmailAddress;
 
-SELECT firstname, lastname
+SELECT DISTINCT LastName 
 FROM person.person;
 
-SELECT DISTINCT firstname
-FROM person.person;
 
-SELECT DISTINCT firstname, lastname 
-FROM person.person;
+SELECT * FROM
+Production.Product;
 
-SELECT * FROM person.Person
-WHERE lastname = 'Miller' and FirstName = 'Anna';
-
-
-SELECT * FROM production.product
-WHERE color = 'blue' or color = 'black';
-
-SELECT * FROM production.product
-WHERE ListPrice > 1500;
-
-SELECT * FROM production.product
-WHERE ListPrice > 1500 and ListPrice < 3000;
-
-SELECT * FROM production.product
-WHERE color <> 'red';
-
-SELECT * FROM production.product
+SELECT * FROM Production.Product
 WHERE Weight > 500 and Weight <= 700;
 
-SELECT * FROM HumanResources.Employee 
-WHERE MaritalStatus = 'm' and SalariedFlag = 'true';
-
-SELECT * FROM Person.Person
-WHERE FirstName = 'Peter' and LastName = 'Krebs';
-
-SELECT * FROM person.EmailAddress
-WHERE BusinessEntityID = 26;
-
-SELECT COUNT(distinct Title) FROM Person.Person
-
-SELECT COUNT(*)
-FROM production.product;
-
-SELECT COUNT(SIZE)
-FROM production.product;
-
-SELECT COUNT(DISTINCT SIZE)
-FROM production.product;
+SELECT * FROM HumanResources.Employee
+WHERE MaritalStatus = 'Married' and SalariedFlag = 'True';
