dfdd0d58843650dbbde2fbbfab5c41a777e7e5c9
JOANNE EMILLY SANTOS DA COSTA <124714961+Joanne-Emilly@users.noreply.github.com>
2023-07-14 11:21:16 -0300

    ATUALIZEI

--- Comandos-IntermediariosBD.sql
+++ Comandos-IntermediariosBD.sql
@@ -4,3 +4,28 @@ FROM Sales.SalesOrderDetail;
 SELECT SpecialOfferID, SUM(UnitPrice) AS "SOMA"
 FROM Sales.SalesOrderDetail
 GROUP BY SpecialOfferID;
+
+-- QUANTOS PRODUTOS FORAM VEDIDOS ATE HOJE?
+
+SELECT ProductID, COUNT(ProductID) AS "CONTAGEM"
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID;
+
+-- QUANTOS NOMES DE CADA NOME TEMOS CADASTRADOS EM NOSSO BD?
+
+SELECT MiddleName, COUNT(MiddleName) AS "QUANTIDADE"
+FROM person.person 
+GROUP BY MiddleName;
+
+
+--QUANTIDADE DE CADA PRODUTO VENDIDO
+
+SELECT * FROM Sales.SalesOrderDetail
+
+Select ProductID, AVG(OrderQty) AS "QUANTIDADE"
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+
+SELECT SpecialOfferID, SUM(UnitPrice) AS "SOMA" 
+FROM Sales.SalesOrderDetail
+GROUP BY SpecialOfferID
